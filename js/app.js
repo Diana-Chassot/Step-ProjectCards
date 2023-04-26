f200701c2f5270c2d60e2e2dd742b1db7cd2a5e4
Diana-Chassot <diana.chassot25@gmail.com>
2023-04-26 16:02:44 +0200

    some changes

--- js/app.js
+++ js/app.js
@@ -1,130 +1,209 @@
-/* Марианна нужно добавить ивент на кнопку с моей функцией postCards() которая будет принимать твои данные */
 
+window.addEventListener('load', getCards);
+
+/* CustomHttp */
 function customHttp() {
+
   const API_TOKEN = "7230c3ef-1075-4f6e-bdbd-9c4639644533";
   const API_URL = "https://ajax.test-danit.com/api/v2/cards";
   return { API_TOKEN, API_URL };
-}
-/* Отправка карточек*/
-function postCards() {
-  const { API_TOKEN, API_URL } = customHttp();
-  fetch(API_URL, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${API_TOKEN}`
-    },
-    body: JSON.stringify({
-      nameClient: "here will be data",
-      nameDoctor: "here will be data",
-      briefVisitDesc: "here will be data",
-      urgency: "here will be data"
-    })
-  })
-    .then(response => checkStatusResponse(response))
-    .then(data => console.log(data))
-}
-/* Получение отправленных карточек */
-function getCards() {
-  const { API_TOKEN, API_URL } = customHttp();
-  fetch(API_URL, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${API_TOKEN}`
-    },
-  })
-    .then(response => checkStatusResponse(response))
-    .then(cards => {
-      cards.forEach(cardItem => {
-        const card = new Card(cardItem);
-        card.renderCard();
-        console.log(card)
-      })
 
-    })
-}
-
-/* checkStatusResponse */
+};
+/* CheckStatusResponse */
 function checkStatusResponse(response) {
+
   if (response.ok) {
     return response.json();
   } else {
     throw new Error('Error');
   }
 };
-/* карточка */
+
+/*Post Cards*/
+async function postCards() {
+
+  try {
+    showSpinner()
+    const { API_TOKEN, API_URL } = customHttp();
+
+    const response = await fetch(API_URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${API_TOKEN}`
+      },
+      body: JSON.stringify({
+        nameClient: "ExapmleName",
+        nameDoctor: "DoctorExample",
+        briefVisitDesc: "here will be data",
+        urgency: "here will be data"
+        /* какие еще данные нужны? */
+      })
+    });
+
+    const card = await checkStatusResponse(response);
+    checkCardsExist(card);
+
+    const newCard = new Card(card);
+
+    newCard.renderCard();
+    hideSpinner()
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+/*Get Cards */
+async function getCards() {
+
+  try {
+    showSpinner()
+    const { API_TOKEN, API_URL } = customHttp();
+
+    const response = await fetch(API_URL, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${API_TOKEN}`
+      },
+    });
+    const cards = await checkStatusResponse(response);
+
+    checkCardsExist(cards);
+    cards.forEach(card => {
+
+      const newCard = new Card(card);
+      newCard.renderCard();
+
+    });
+    hideSpinner()
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+/* Check if cards exist  */
+function checkCardsExist(cards) {
+
+  if (cards.length === 0) {
+    addNoItemsMessage()
+  } else {
+    deleteNoItemsMessage()
+  }
+};
+/* Add Message "no item has been added" */
+function addNoItemsMessage() {
+
+  const cardsContent = document.querySelector(".cards-content");
+  const message = `
+  <span class="no-items">No items have been added</span>
+  `;
+  cardsContent.insertAdjacentHTML("afterbegin", message)
+};
+/* Delete Message "no item has been added" */
+function deleteNoItemsMessage() {
+
+  const noItemsMessage = document.querySelector('.no-items');
+  if (noItemsMessage) {
+    noItemsMessage.remove();
+  }
+};
+/* Show Spinner */
+function showSpinner() {
+  const spinner = document.querySelector(".spinner");
+  spinner.style.display = "block";
+};
+/* Hide Spinner */
+function hideSpinner() {
+  const spinner = document.querySelector(".spinner");
+  spinner.style.display = "none";
+};
+/* Card class and methodes delete, edit, render  */
 class Card {
-  constructor({ nameClient, nameDoc, briefVisitDescr, urgency, id }) {
+
+  constructor({ nameClient, nameDoc, briefVisitDescr, urgency, id /* больше данных */ }) {
     this.nameClient = nameClient;
     this.nameDoctor = nameDoc;
     this.briefVisitDescription = briefVisitDescr;
     this.urgency = urgency;
     this.id = id;
-
-    /* кнопки будущие */
-      
-   
-
   }
-  /* удаление карточки . не доделано */
-  deleteCard() {
-    const { API_TOKEN, API_URL } = customHttp();
-    fetch(API_URL, {
-      method: 'DELETE',
-      headers: {
-        'Authorization': `Bearer ${API_TOKEN}`
-      },
-    })
+  /* delete card */
+  async deleteCard() {
+
+    try {
+      showSpinner()
+      const { API_TOKEN, API_URL } = customHttp();
+      const API_URL_ID = `${API_URL}/${this.id}`;
+
+      const response = await fetch(API_URL_ID, {
+        method: 'DELETE',
+        headers: {
+          'Authorization': `Bearer ${API_TOKEN}`
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error('Failed to delete card');
+      }
+
+      this.element.remove();
+      hideSpinner()
+    }
+    catch (error) {
+      console.error(error);
+    }
   }
   /* корректировка карточки. не доделано*/
   editCard() {
-    
+    console.log(this.id);
   }
   /* разметка карточки */
   templateCard() {
+
     const card = `
-    <div class="col-xl" style="max-width: 15rem">
-      <div class="card border-warning text-center" id="${this.id}">
-        <div class="card-header border-warning bg-dark">
-          <h2 class="card-title text-uppercase text-warning">New visit</h2>
-          <button type="button" class="btn-close btn-delete "></button>
-        </div>
-        <div class="card-body">
-          <p class="card-subtitle mb-2 text-body-secondary">${this.nameClient}</p>
-          <p class="card-text">${this.nameDoctor}</p>
-          <div>
-            <button class="btn">
-              <span class="text-uppercase text-warning">Edit card</span>
-            </button>
-            <button class="btn btn-sm btn-warning" type="button" data-bs-toggle="collapse"
-              data-bs-target="#c${this.id}" aria-expanded="false"
-              aria-controls="c${this.id}">
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor"
-                class="bi bi-arrow-down-square" viewBox="0 0 16 16">
-                <path fill-rule="evenodd"
-                  d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm8.5 2.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V4.5z" />
-              </svg>
-            </button>
-          </div>
-          <div id="c${this.id}" class="collapse">
-            <div class="card-more-info">
+      <div class="card-element" style="max-width: 16rem">
+        <div class="card border-warning shadow text-center col-xl " id="${this.id}">
+            <div class="card-header border-warning bg-dark">
+                <h2 class="card-title text-uppercase text-warning">New visit</h2>
+                <button type="button" class="btn-delete btn-close btn-close-white"></button>
+            </div>
+            <div class="card-body">
+              <p>${this.nameClient}</p>
+              <p>${this.nameDoctor}</p>
+              <button class="btn btn-edit btn-dark">
+                <span class="text-uppercase text-warning">Edit card</span>
+              </button>
+              <button class="btn btn-warning" type="button" data-bs-toggle="collapse"
+                data-bs-target="#c${this.id}" aria-controls="c${this.id}">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" 
+                fill="currentColor"class="bi-arrow-down-square" 
+                viewBox="0 0 16 16">
+                  <path fill-rule="evenodd"
+                  d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 
+                  2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm8.5 2.5a.5.5 0 0 0-1 
+                  0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 
+                  3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V4.5z" />
+                </svg>
+              </button>
+            </div>
+            <div id="c${this.id}" class="collapse">
+              <p>
               контент карточки, который будет показан при
               раскрытии.
+              </p>
             </div>
-          </div>
+          </div>    
         </div>
-      </div>
-    </div>
-    `
+      `
     return card;
-  }
+  };
   /* добавление карточки в разметку */
   renderCard() {
     const cardsContent = document.querySelector(".cards-content");
 
     let fragment = this.templateCard();
-    cardsContent.insertAdjacentHTML("afterbegin", fragment)
+
+    cardsContent.insertAdjacentHTML("afterbegin", fragment);
   }
-}
-getCards()
+};
