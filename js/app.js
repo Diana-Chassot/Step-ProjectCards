55f905c5d54e2b6553889969ff12ae337e375c32
Marianna Haleliuka <marianna.galelyuka@gmail.com>
2023-04-26 18:49:07 +0300

    dropdown for selecting a doctor created

--- js/app.js
+++ js/app.js
@@ -34,3 +34,20 @@ logInModalBtn.addEventListener('click', () => {
         visitsNoItem.classList.toggle('hidden-element');
     }
 })
+
+
+const createVisitModal = document.getElementById('createVisitModal')
+if (createVisitModal) {
+    createVisitModal.addEventListener('show.bs.modal', event => {
+    // Button that triggered the modal
+    const button = event.relatedTarget
+    // Extract info from data-bs-* attributes
+    const recipient = button.getAttribute('data-bs-whatever')
+    // If necessary, you could initiate an Ajax request here
+    // and then do the updating in a callback.
+
+    // Update the modal's content.
+    // const modalBodyInput = logInModal.querySelector('.modal-body input')
+    // modalBodyInput.value = recipient
+    })
+}
