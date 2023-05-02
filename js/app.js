6c27d39ce55ec2a5b1688168669526de895a6cba
Marianna Haleliuka <marianna.galelyuka@gmail.com>
2023-05-02 17:49:20 +0300

    modules "modal", "visit" added

--- js/app.js
+++ js/app.js
@@ -435,49 +435,6 @@ createVisitConfirmBtn.addEventListener('click', (e) => {
     }
 })
 
-// function createNewVisit() {
-//     let newVisit;
-
-//     if (doctorSelect.value === 'therapist') {
-//         newVisit = new VisitTherapist(
-//             document.getElementById('visit-patient').value,
-//             document.getElementById('doctor').value,
-//             document.getElementById('visit-purpose').value,
-//             document.getElementById('visit-description').value,
-//             document.getElementById('urgency').value,
-//             document.getElementById('visit-age').value
-//         )
-//     }
-
-//     if (doctorSelect.value === 'dentist') {
-//         newVisit = new VisitDentist(
-//             document.getElementById('visit-patient').value,
-//             document.getElementById('doctor').value,
-//             document.getElementById('visit-purpose').value,
-//             document.getElementById('visit-description').value,
-//             document.getElementById('urgency').value,
-//             document.getElementById('visit-last').value
-//         )
-//     }
-
-//     if (doctorSelect.value === 'cardiologist') {
-//         newVisit = new VisitCardiologist(
-//             document.getElementById('visit-patient').value,
-//             document.getElementById('doctor').value,
-//             document.getElementById('visit-purpose').value,
-//             document.getElementById('visit-description').value,
-//             document.getElementById('urgency').value,
-//             document.getElementById('visit-age').value,
-//             document.getElementById('visit-BMI').value,
-//             document.getElementById('visit-normal-pressure').value,
-//             document.getElementById('visit-heart-diseases').value
-//         )
-//     }
-
-//     const {nameClient, doctor, purposeOfTheVisit, briefVisitDescr, urgency, age, bodyMassIndex, bloodPressure, pastDiseasesCardiovascularSystem, dateOfLastVisit} = newVisit;
-//     return {nameClient, doctor, purposeOfTheVisit, briefVisitDescr, urgency, age, bodyMassIndex, bloodPressure, pastDiseasesCardiovascularSystem, dateOfLastVisit}
-// }
-
 /* скасування створення візиту*/
 const createVisitCancelBtn = document.getElementById('create-visit-cancel');
 createVisitCancelBtn.addEventListener('click', (e) => {
